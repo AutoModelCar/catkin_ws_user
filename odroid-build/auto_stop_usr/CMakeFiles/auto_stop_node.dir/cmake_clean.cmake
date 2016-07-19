1b38dfd960d93a28f90429dbfbc64823f22313a8
boroujeni <boroujeni@mi.fu-berlin.de>
2016-07-19 23:48:20 +0200

    .catkin .rosinstall

---
+++ odroid-build/auto_stop_usr/CMakeFiles/auto_stop_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/auto_stop_node.dir/src/auto_stop.cpp.o"
+  "/home/zargol/catkin_ws_user/odroid-devel/lib/auto_stop_usr/auto_stop_node.pdb"
+  "/home/zargol/catkin_ws_user/odroid-devel/lib/auto_stop_usr/auto_stop_node"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/auto_stop_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ odroid-build/auto_stop_usr/catkin_generated/pkg.installspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "std_msgs;sensor_msgs;geometry_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lauto_stop_usr".split(';') if "-lauto_stop_usr" != "" else []
+PROJECT_NAME = "auto_stop_usr"
+PROJECT_SPACE_DIR = "/home/zargol/catkin_ws_user/odroid-install"
+PROJECT_VERSION = "0.0.0"
