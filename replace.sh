13c796b4b2edea4eab3f2ea4a02980aef755f521
Daniel Neumann <daniel.neumann@fu-berlin.de>
2016-07-20 23:53:54 +0200

    fixed replace.sh script

--- replace.sh
+++ replace.sh
@@ -1,9 +1,7 @@
-
-
 echo "replace root path"
 var=$(pwd)
 tmpfile=/tmp/magicReplaceTempFile
-target=root@192.168.1.199:./catkin_ws_user/
+target=root@192.168.43.112:./catkin_ws_user/
 echo "The catkin_ws_user directory is $var."
 
 
@@ -13,19 +11,19 @@ rsync -r $var/odroid-build $target
 
 sed 's#'$var'#/root#'g $var/odroid-devel/_setup_util.py > $tmpfile
 sed -i 's#/opt/odroid-x2/sdk/#/#'g $tmpfile
+sed -i 's#/catkin_ws/#/catkin_ws_user/#'g $tmpfile
 sed -i 's#/opt/ros/indigo;//opt/ros/indigo#/opt/ros/indigo;/root/catkin_ws/devel/#'g $tmpfile
 rsync $tmpfile $target/odroid-devel/_setup_util.py
 
-sed 's#'$var'#/root#'g $var/odroid-build/.catkin_tools.yaml > $tmpfile
+sed 's#'$var'#/root/catkin_ws_user#'g $var/odroid-build/.catkin_tools.yaml > $tmpfile
 rsync $tmpfile $target/odroid-build/.catkin_tools.yaml
 
-sed 's#'$var'#/root#'g $var/odroid-devel/.catkin > $tmpfile
+sed 's#'$var'#/root/catkin_ws_user#'g $var/odroid-devel/.catkin > $tmpfile
 rsync $tmpfile $target/odroid-devel/.catkin
 
-sed 's#'$var'#/root#'g $var/odroid-devel/setup.sh > $tmpfile
+sed 's#'$var'#/root/catkin_ws_user#'g $var/odroid-devel/setup.sh > $tmpfile
 rsync $tmpfile $target/odroid-devel/setup.sh
 
-sed 's#'$var'#/root#'g $var/odroid-devel/.rosinstall > $tmpfile
+sed 's#'$var'#/root/catkin_ws_user#'g $var/odroid-devel/.rosinstall > $tmpfile
 rsync $tmpfile $target/odroid-devel/.rosinstall
 echo "finished"
-
