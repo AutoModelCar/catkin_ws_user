a028dd0216056e6a892b86a712c281bff000a6d8
Lutz <lutz.freitag@fu-berlin.de>
2016-07-20 17:17:45 -0500

    whopsie... removed those echos

--- replace.sh
+++ replace.sh
@@ -14,17 +14,17 @@ sed 's#'$var'#/root#'g $var/odroid-devel/_setup_util.py > $tmpfile
 sed -i 's#/opt/odroid-x2/sdk/#/#'g $tmpfile
 sed -i 's#/catkin_ws/#/'$ws_name'/#'g $tmpfile
 sed -i 's#/opt/ros/indigo;//opt/ros/indigo#/opt/ros/indigo;/root/catkin_ws/devel/#'g $tmpfile
-echo rsync $tmpfile $target/odroid-devel/_setup_util.py
+rsync $tmpfile $target/odroid-devel/_setup_util.py
 
 sed 's#'$var'#/root/'$ws_name'#'g $var/odroid-build/.catkin_tools.yaml > $tmpfile
-echo rsync $tmpfile $target/odroid-build/.catkin_tools.yaml
+rsync $tmpfile $target/odroid-build/.catkin_tools.yaml
 
 sed 's#'$var'#/root/'$ws_name'#'g $var/odroid-devel/.catkin > $tmpfile
-echo rsync $tmpfile $target/odroid-devel/.catkin
+rsync $tmpfile $target/odroid-devel/.catkin
 
 sed 's#'$var'#/root/'$ws_name'#'g $var/odroid-devel/setup.sh > $tmpfile
-echo rsync $tmpfile $target/odroid-devel/setup.sh
+rsync $tmpfile $target/odroid-devel/setup.sh
 
 sed 's#'$var'#/root/'$ws_name'#'g $var/odroid-devel/.rosinstall > $tmpfile
-echo rsync $tmpfile $target/odroid-devel/.rosinstall
+rsync $tmpfile $target/odroid-devel/.rosinstall
 echo "finished"
