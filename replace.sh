c403db1daa38f2eaf6b05559eb0a216721d9e146
Daniel Neumann <daniel.neumann@fu-berlin.de>
2016-07-20 17:54:42 +0200

    changed replace.sh

--- replace.sh
+++ replace.sh
@@ -5,6 +5,7 @@ var=$(pwd)
 echo "The catkin_ws_user directory $var."
 sed -i 's#'$var'#/root#'g $var/catkin_ws_user/odroid-devel/.catkin $var/catkin_ws_user/odroid-devel/setup.sh $var/catkin_ws_user/odroid-devel/_setup_util.py $var/catkin_ws_user/odroid-devel/.rosinstall
 sed -i 's#/opt/odroid-x2/sdk/#/#'g $var/catkin_ws_user/odroid-devel/_setup_util.py
+sed -i 's#/opt/ros/indigo;//opt/ros/indigo#/opt/ros/indigo;/root/catkin_ws/devel/#'g $var/catkin_ws_user/odroid-devel/_setup_util.py
 sed -i 's#'$var'#/root#'g $var/catkin_ws_user/odroid-build/.catkin_tools.yaml
 scp -r $var/catkin_ws_user/src/ root@192.168.1.199:./catkin_ws_user/
 scp -r $var/catkin_ws_user/odroid-devel/ root@192.168.1.199:./catkin_ws_user/
