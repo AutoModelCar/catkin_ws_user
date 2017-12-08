52d11ad6004b8e882eccadea49c97ba6953b8823
Zahra Boroujeni <boroujeni@kawasaki.imp.fu-berlin.de>
2017-12-08 18:45:31 +0100

    load dynamic matrix

--- src/fu_navigation/src/path_planning_plot.py
+++ src/fu_navigation/src/path_planning_plot.py
@@ -4,13 +4,13 @@ import numpy as np
 import path_parser
 
 import matplotlib.pyplot as plt
-
+import matplotlib.patches as mpatches
 from scipy.spatial import KDTree
 map_size_x=600 #cm
 map_size_y=400 #cm
 resolution = 10 # cm
 matrix = np.zeros( (map_size_x/resolution,map_size_y/resolution,2),dtype='f' )
-
+yaw = -np.pi/3
 def main(map_file):
     xy = np.array(list(path_parser.read_points(map_file)))
     x, y = xy.T
@@ -18,14 +18,15 @@ def main(map_file):
     fig = plt.figure(figsize=(12, 10), facecolor='w')
     plt.plot(x, y, ':o', markersize=2)
     global matrix
-    matrix = np.load('matrix1.npy')
+    matrix = np.load('matrixDynamic.npy')
 
     plt.gca().set_aspect(1, 'datalim')  # keep circles as circles
     plt.tight_layout()
 
     def show_nearest(target):
 
-        yaw=0 #np.pi/2
+        global yaw
+        yaw+=np.pi/3 #np.pi/2
         car_length=0.3
 
         global matrix
@@ -35,12 +36,19 @@ def main(map_file):
         y_index=np.int(y1*resolution)
         print(x_index,y_index)
         x3, y3 = matrix[x_index,y_index,:]
-        print(x3,y3)
         f_x=np.cos(yaw)*x3 + np.sin(yaw)*y3
         f_y=-np.sin(yaw)*x3 + np.cos(yaw)*y3
+        Kp=4
+        steering=Kp*np.arctan(f_y/(4.0*f_x))
 
-        steering=np.arctan2(f_y,f_x)
-        
+        if (f_x>0):
+            speed = -150
+        else:
+            speed = 150
+            if (f_y>0):
+                steering = -np.pi/2
+            if (f_y<0):
+                steering = np.pi/2
 
         if (steering>(np.pi)/4):
             steering = (np.pi)/4
@@ -59,8 +67,22 @@ def main(map_file):
         xc = x1 - np.sin(yaw) * r
         yc = y1 + np.cos(yaw) * r
 
+    
+
+
         ax = plt.axes()
-        ax.arrow(x1, y1, np.cos(yaw)*car_length, np.sin(yaw)*car_length, head_width=0.05, head_length=0.1, fc='r', ec='r')
+        ax.arrow(x1, y1, car_length*np.cos(yaw), car_length*np.sin(yaw), width=car_length, head_width=car_length, head_length=0.09, fc='b', ec='b')
+
+        ax = plt.axes()
+        ax.arrow(x1, y1, f_x*np.cos(yaw), f_x*np.sin(yaw), head_width=0.01, head_length=0.01, fc='r', ec='r')
+
+        ax = plt.axes()
+        ax.arrow(x1, y1, -f_y*np.sin(yaw), f_y*np.cos(yaw), head_width=0.01, head_length=0.01, fc='r', ec='r')
+
+        ax = plt.axes()
+        ax.arrow(x1, y1, x3, y3, head_width=0.01, head_length=0.01, fc='g', ec='g')
+
+
 
         plt.scatter(*target, color='r')
         plt.scatter(*(x1 + x3, y1 + y3), color='g')
